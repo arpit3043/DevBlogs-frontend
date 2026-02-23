d87d3434d22edd094de85f289fb6c18e9f36c1e3
i-am-abs <abs.createx@gmail.com>
2026-02-24 00:13:20 +0530

    added vercel.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import {Route, Switch} from "wouter";
+import {Analytics} from "@vercel/analytics/react";
 import {queryClient} from "./lib/queryClient";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {Toaster} from "@/components/ui/toaster";
@@ -45,6 +46,7 @@ function App() {
                 <TooltipProvider>
                     <Toaster/>
                     <Router/>
+                    <Analytics/>
                 </TooltipProvider>
             </QueryClientProvider>
         </ThemeProvider>
